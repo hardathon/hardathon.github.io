7a8446a8de96d65f3361266f246e5339b6c7e723
vladimir.akritskiy <vladimir.akritskiy@onetwotrip.com>
2017-11-23 00:03:17 +0400

    Ruturn to old design

--- js/hardathon.js
+++ js/hardathon.js
@@ -4,6 +4,7 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.move-section-down').bind('click', function(event) {
         $.fn.fullpage.moveSectionDown();
@@ -14,39 +15,11 @@ $(function() {
 // fullPage initialization
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        autoScrolling:false,
-        allowPageScroll: true,
-        fitToSection: false,
-        scrollingSpeed: 500,
-        normalScrollElements: '#main, #about, #how, #where, #faq, #contacts',
+    	scrollingSpeed: 1000,    	
     	responsiveWidth: 300,
         responsiveHeight: 400,
         paddingBottom: '10px',
         paddingTop: '10px',
         controlArrowColor: '#424242',
-        menu: '#menu',
-        anchors: ['main', 'about', 'how', 'where', 'faq', 'contacts']
     });
-
-    var count=0;
-    $("body").on('keypress', function(e) {
-          if (e.which == 1099 ||  e.which == 1067 ||  e.which ==  83 ||  e.which == 115) {
-              count++;
-              if(count===5) {
-                $('.kitty').addClass('active');
-                setTimeout(function() { $('.kitty').removeClass('active') }, 5000);
-                count=0;
-              }
-          }
-     });
 });
-
-ymaps.ready(init);
-
-function init () {
-  var myMap = new ymaps.Map('map', {
-    center: [55.746293, 37.584994],
-    zoom: 13
-  });
-  myMap.behaviors.disable('scrollZoom');
-}
