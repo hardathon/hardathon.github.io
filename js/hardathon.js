a19c013448d25cf1fac5b5b687aa64bf485b0a30
vladimir.akritskiy <vladimir.akritskiy@onetwotrip.com>
2017-11-23 05:47:23 +0400

    fix

--- js/hardathon.js
+++ js/hardathon.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
         controlArrowColor: '#424242',
     });
 
-    $('.intro').wallpaper({
+    $('.intro .header-video').wallpaper({
         source: {
             mp4: '/video/masthead-teaser.mp4',
             ogg: '/video/masthead-teaser.ogv'
